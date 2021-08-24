d617c7688576a1fbe6c862b13578028bedc839be
nozomi0511 <72549576+saibaimax@users.noreply.github.com>
2021-08-24 23:07:55 +0900

    Initial commit

---
+++ src/typeList.tsx
@@ -0,0 +1,40 @@
+/* enlisnt-disable @typescript-eslint/no-unused-vars */
+
+/**TypeScript の基本の型 */
+
+// boolean
+let bool: boolean = true;
+
+// number 
+let num: number = 0;
+
+// string
+let str: string = "A";
+
+// Array 
+let arr1: Array<number> = {0, 1, 2};
+let arr2: number{} = {0, 1, 2};
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "TEST";
+}
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object 
+let obj1: object = {};
+let obj2: { id: number, name: string } = { id: 0, name: "AAA" };
+
+
+
